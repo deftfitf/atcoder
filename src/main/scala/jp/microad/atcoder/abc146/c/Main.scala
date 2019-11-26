ae1c19f9d52017fbffb6999c82d0968f2cee1262
ishida <ishida_junya@microad.co.jp>
2019-11-26 15:37:41 +0900

    abc 146 a-c

---
+++ src/main/scala/jp/microad/atcoder/abc146/c/Main.scala
@@ -0,0 +1,20 @@
+package jp.microad.atcoder.abc146.c
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+
+    val A, B, X = sc.nextLong()
+
+    def binarySearch(l: Long, r: Long): Long =
+      if (r - l > 1) {
+        val N = l + (r - l) / 2
+        if ((A * N + B * N.toString.length) > X) binarySearch(l, N)
+        else binarySearch(N, r)
+      } else l
+
+    println(binarySearch(0, Math.pow(10, 9).toLong + 1))
+  }
+
+}
