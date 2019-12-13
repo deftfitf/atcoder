1537005f632ee29104bc0d09272d4cf570dbf5cf
deftfitf <nextframeworks@gmail.com>
2019-12-14 01:30:50 +0900

    abc142

---
+++ src/main/scala/jp/microad/atcoder/abc142/a/Main.scala
@@ -0,0 +1,12 @@
+package jp.microad.atcoder.abc142.a
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+
+    val N = sc.nextDouble()
+    println(Math.ceil(N / 2.0) / N)
+  }
+
+}
---
+++ src/main/scala/jp/microad/atcoder/abc142/c/Main.scala
@@ -0,0 +1,14 @@
+package jp.microad.atcoder.abc142.c
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+
+    val N = sc.nextInt()
+    val A = Array.fill(N)(sc.nextInt())
+
+    println((0 until N) sortBy A map (_ + 1) mkString(" "))
+  }
+
+}
