3b165907c801fb781c591477242a34e0c9d241fa
deftfitf <nextframeworks@gmail.com>
2019-12-20 23:00:17 +0900

    hoge

--- src/main/scala/jp/microad/atcoder/abc094/d/Main.scala
+++ src/main/scala/jp/microad/atcoder/abc094/d/Main.scala
@@ -1,13 +1,12 @@
 package jp.microad.atcoder.abc094.d
 
-// TODO: まだ
 object Main {
 
   def main(args: Array[String]): Unit = {
     val sc = new java.util.Scanner(System.in)
 
     val N = sc.nextInt()
-    val A = Array.fill(N)(sc.nextInt())
+    val A = Array.fill(N)(sc.nextInt()).sorted
 
     // comb(ai, aj), これは ai を固定した時には,
     // 単峰関数のような関数 f_ai(x) = comb(ai, x)
@@ -16,46 +15,47 @@ object Main {
     // これは, 二分探索で探せるのでいける
     // これを全てのaiについて調べても間に合う
     // 実際に combを計算する必要はない
+    // ... 全ての ai について調べると, どうやらそうでないケースがある
+    // 最大の ai について調べるだけでいい
     def solve(N: Int, A: Array[Int]): (Int, Int) = {
-      // aj > n となる 最小の aj を返す
-      def upperBounds(n: Int): Int = {
+      def upperBounds(n: Int, upper: Int): Int = {
         @scala.annotation.tailrec
         def loop(left: Int, right: Int): Int =
           if (right - left > 1) {
             val mid = left + (right - left) / 2
-            if (A(mid) > n) loop(left, mid)
+            if (A(mid) >= n) loop(left, mid)
             else loop(mid, right)
           } else right
-        loop(-1, N)
+        loop(-1, upper)
       }
-      // aj <= n となる最大の aj を 返す
-      def lowerBounds(n: Int): Int = {
+      def lowerBounds(n: Int, upper: Int): Int = {
         @scala.annotation.tailrec
         def loop(left: Int, right: Int): Int =
           if (right - left > 1) {
             val mid = left + (right - left) / 2
-            if (A(mid) <= n) loop(left, mid)
-            else loop(mid, right)
+            if (A(mid) <= n) loop(mid, right)
+            else loop(left, mid)
           } else left
-        loop(0, N)
+        loop(-1, upper)
       }
-
       @scala.annotation.tailrec
       def loop(i: Int, diff: Int, n: Int, r: Int): (Int, Int) =
         if (i < N) {
-          val center = A(i) / 2
-          val left = A(lowerBounds(center))
-          val right = A(upperBounds(center))
-          if (center - left > right - center) {
-            if (center - left > diff) loop(i + 1, center - left, A(i), left)
+          val center = (A(i) / 2.toDouble).round.toInt
+          val li = lowerBounds(center, i)
+          val ri = upperBounds(center, i)
+          val left = if (li >= 0) A(li) else 0
+          val right = if (ri < A(i)) A(ri) else A(i)
+          if (left > A(i) - right) {
+            if (left > diff) loop(i + 1, left, A(i), left)
             else loop(i + 1, diff, n, r)
           } else {
-            if (right - center > diff) loop(i + 1, right - center, A(i), right)
+            if (A(i) - right > diff) loop(i + 1, A(i) - right, A(i), right)
             else loop(i + 1, diff, n, r)
           }
         } else (n, r)
 
-      loop(0, 0, 0, 0)
+      loop(N - 1, 0, A.last, 0)
     }
 
     val (n, r) = solve(N, A)
@@ -63,3 +63,4 @@ object Main {
   }
 
 }
+
