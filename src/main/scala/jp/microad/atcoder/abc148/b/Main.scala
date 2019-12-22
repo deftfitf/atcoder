855e412c0e0f37be80314a261b0985f52e17c1d7
deftfitf <nextframeworks@gmail.com>
2019-12-23 08:34:44 +0900

    abc148

---
+++ src/main/scala/jp/microad/atcoder/abc148/b/Main.scala
@@ -0,0 +1,16 @@
+package jp.microad.atcoder.abc148.b
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+
+    val N = sc.nextInt(); sc.nextLine()
+    val _S = sc.nextLine().split(" ")
+    val S = _S(0)
+    val T = _S(1)
+    val r = S.zip(T).map(z => z._1.toString ++ z._2.toString).reduce(_ ++ _)
+    println(r)
+  }
+
+}
---
+++ src/main/scala/jp/microad/atcoder/abc148/c/Main.scala
@@ -0,0 +1,20 @@
+package jp.microad.atcoder.abc148.c
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+
+    val A, B = sc.nextInt()
+
+    def gcd(a: Long, b: Long): Long =
+      if (a % b == 0) b
+      else gcd(b, a % b)
+
+    def lcm(a: Long, b: Long): Long =
+      a * (b / gcd(a, b))
+
+    println(lcm(A, B))
+  }
+
+}
