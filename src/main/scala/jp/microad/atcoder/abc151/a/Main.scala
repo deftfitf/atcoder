bc12085827ed59cc469b4f82f64cf7f29378fbf5
deftfitf <nextframeworks@gmail.com>
2020-01-25 17:33:58 +0900

    abc151

---
+++ src/main/scala/jp/microad/atcoder/abc151/a/Main.scala
@@ -0,0 +1,12 @@
+package jp.microad.atcoder.abc151.a
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val sc = new java.util.Scanner(System.in)
+
+    val C: Char = (sc.nextLine()(0) + 1).toChar
+    println(C)
+  }
+
+}
